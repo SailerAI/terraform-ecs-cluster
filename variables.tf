f19be35895ed63f7a5541c8ad2d85040d715e24d
Pablo <pmenezes.aju@gmail.com>
2025-01-19 20:51:08 -0300

    Feat: Module ECS Cluster v1

---
+++ variables.tf
@@ -0,0 +1,60 @@
+## Global Variables
+
+variable "project_name" {
+  description = "Nome do projeto"
+  type        = string
+}
+
+variable "environment" {
+  description = "Ambiente"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+## Variables VPCs SSM STORE
+
+variable "ssm_vpc_id" {
+  description = "ID do VPC"
+  type        = string
+}
+
+variable "ssm_public_subnet_ids" {
+  description = "IDs das subredes públicas"
+  type        = list(string)
+  sensitive   = false
+}
+
+variable "ssm_private_subnet_ids" {
+  description = "IDs das subredes privadas"
+  type        = list(string)
+}
+
+variable "ssm_database_subnet_ids" {
+  description = "IDs das subredes de banco de dados"
+  type        = list(string)
+}
+
+## Variables Load Balancer
+
+variable "load_balancer_internal" {
+  description = "Se o load balancer é interno"
+  type        = bool
+}
+
+variable "load_balancer_type" {
+  description = "Tipo do load balancer"
+  type        = string
+}
+
+
+##### ECS Variables
+
+variable "capacity_providers" {
+  description = "Capacity providers"
+  type        = list(string)
+  default     = ["FARGATE", "FARGATE_SPOT"]
+}
